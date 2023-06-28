513129ba3602bc145dc1cbbdb08fcffa2f47e9e2
AsifBashir <56689372+Asif26@users.noreply.github.com>
2023-06-28 20:26:22 +0500

    added butoon and handle func

--- my-react-class/src/App.js
+++ my-react-class/src/App.js
@@ -1,31 +1,5 @@
-import Gallery from './components/gallery';
-import './App.css';
-import Todo from './components/todo';
-import Avatar from './components/Avatar';
-import TodoList from './components/ToDoList';
-import Profile from './components/profile';
-import { getImageUrl } from './components/utils';
-import PackingList from './components/PackingList';
-import List from './components/List';
+import Button from "./components/Button";
 
-function App() {
-  return (
-    <div >
-      <h1 className='App'>Amzing Scientists</h1>
-      {/* <Gallery />
-      <Gallery />
-      <Gallery />
-      <Gallery /> */}
-      {/* <Todo /> */}
-      {/* <Avatar /> */}
-      {/* <TodoList /> */}
-      {/* <Profile /> */}
-      {/* <PackingList /> */}
-      <List />
-
-
-    </div>
-  );
+export default function App(){
+  return <Button />
 }
-
-export default App;
