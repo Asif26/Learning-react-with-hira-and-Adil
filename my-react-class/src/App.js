f0c45cfc72c2ab8ed947f0a580247c5aa2cbf943
AsifBashir <56689372+Asif26@users.noreply.github.com>
2023-07-02 20:18:18 +0500

    hard work

--- my-react-class/src/App.js
+++ my-react-class/src/App.js
@@ -1,8 +1,21 @@
 
 import Button from "./components/Button";
 import Toolbar from "./components/Tolbar";
-
-export default function App(){
-  return(<> <Button /> 
-  <Toolbar/> </>);
+import Clock from "./components/Clock";
+import Gallery from "./components/gallery";
+// import Form from "./components/Form";
+import Form from "./components/handelSubmit";
+import Counter from "./components/Counter";
+export default function App() {
+  return (<section>
+    <Counter />
+    <Form />
+     {/* <Clock /> 
+     <Form /> 
+     <Gallery /> 
+    <Gallery /> 
+    <Gallery />
+    <Gallery />  */}
+  </section>
+    );
 }
